a19ac70389bacb73972d8b4485af033588029437
cjanie <janiechun@hotmail.fr>
2022-05-19 12:56:19 +0200

    go to settings when permission is denied

--- app/src/main/java/com/android/sig/ui/fragments/StartFragment.kt
+++ app/src/main/java/com/android/sig/ui/fragments/StartFragment.kt
@@ -2,24 +2,28 @@ package com.android.sig.ui.fragments
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.PendingIntent
+import android.content.ActivityNotFoundException
+import android.content.DialogInterface
 import android.content.Intent
 import android.location.Location
+import android.net.Uri
 import android.os.Bundle
 import android.os.Looper
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.android.sig.R
 import com.android.sig.viewmodels.SharedViewModel
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
+import com.android.sig.BuildConfig
 import com.android.sig.Launch
 import com.android.sig.ui.MainActivity
 import com.google.android.gms.location.*
@@ -44,6 +48,7 @@ class StartFragment: Fragment() {
         this.point.setOnClickListener {
             this.handleClickPoint()
         }
+
         return root
     }
 
@@ -72,7 +77,7 @@ class StartFragment: Fragment() {
                     fusedLocationProviderClient.lastLocation.addOnCompleteListener { task ->
                         val lastLocation: Location = task.result
                         this.saveLocation(lastLocation)
-                        this.navigateToNextStep()
+                        this.goToNextStep()
                     }
 
                 } else {
@@ -84,10 +89,10 @@ class StartFragment: Fragment() {
             }
 
         } else {
-            this.showMessage("Permission is denied")
+            //Permission is denied
+            this.goToSettings()
         }
     }
-
 
     private fun handleClickPoint() {
         this.activityResultLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
@@ -130,8 +135,33 @@ class StartFragment: Fragment() {
         Toast.makeText(this.context, message, Toast.LENGTH_LONG).show()
     }
 
-    private fun navigateToNextStep() {
+    private fun goToNextStep() {
         this.findNavController().navigate(R.id.action_startFragment_to_nameFragment)
     }
+
+    private fun goToSettings() {
+        val positiveButtonClick = { dialog: DialogInterface, which: Int ->
+            val intent = Intent()
+            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            val uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+            intent.setData(uri)
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            try {
+                this.startActivity(intent)
+            } catch (e: ActivityNotFoundException) {
+                this.showMessage(e.message.toString())
+                e.printStackTrace()
+            }
+        }
+
+        AlertDialog.Builder(this.requireContext())
+            .setTitle(R.string.go_to_settings)
+            .setMessage(R.string.request_location_permission)
+            .setPositiveButton(R.string.ok, positiveButtonClick)
+            .create().show()
+
+    }
+
+
 
 }
